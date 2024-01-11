3f8783a2b49217cbf2d59db5d8e3ed243ad98dbd
LorussoDanilo <60606528+LorussoDanilo@users.noreply.github.com>
2024-01-11 10:13:16 +0100

    TO-DO: Bisogna far funzionare il bot dopo aver fatto tutte le domande. Dunque, passare le risposte a chatgpt.

--- handle_user_data.py
+++ handle_user_data.py
@@ -1,4 +1,5 @@
-from utils.connection import connect_mysql
+from chatGPT_response import write_chatgpt
+from utils.connection import connect_mysql, connect
 
 
 def create_new_user(telegram_id, username):
@@ -65,68 +66,72 @@ def get_all_telegram_ids(mysql_cursor):
     return [row[0] for row in result]
 
 
-def ask_user_info(telegram_id, bot, questions_and_fields, state):
-    state = {
-        'current_index': 0,
-        'user_state': {field: False for _, field in questions_and_fields}
-    }
+def ask_user_info(telegram_id, bot, questions_and_fields):
+    index = 0  # Indice della domanda corrente
+    interview_complete = False  # Indica se l'intervista è stata completata
 
-    current_question, current_field = questions_and_fields[state['current_index']]
+    def ask_next_question():
+        nonlocal index, interview_complete
+        if not interview_complete and index < len(questions_and_fields):
+            question, field = questions_and_fields[index]
+            bot.send_message(telegram_id, question)
+        else:
+            # Se l'intervista è già stata completata, non fare nulla
+            pass
+
+    def send_completion_message():
+        bot.send_message(telegram_id, "Tutte le domande sono state risposte. Grazie!")
+
+
 
     @bot.message_handler(func=lambda message: message.chat.id == telegram_id)
     def handle_user_response(message):
+        nonlocal index, interview_complete
         try:
-            nonlocal current_question, current_field
-            user_response = str(message.text)  # Assicurati che user_response sia una stringa
-            telegram_user_id = message.chat.id
+            if not interview_complete:
+                user_response = str(message.text)
+                telegram_user_id = message.chat.id
 
-            # Connessione a MySQL e ottenimento di un cursore
-            mysql_connection, cursor = connect_mysql()
+                # Connessione a MySQL e ottenimento di un cursore
+                mysql_connection, cursor = connect_mysql()
 
-            # Esecuzione della query per aggiornare il profilo dell'utente nel database
-            update_query = f"UPDATE utenti SET {current_field} = %s WHERE telegram_id = %s"
-            cursor.execute(update_query, (user_response, telegram_user_id))
+                # Esecuzione della query per aggiornare il profilo dell'utente nel database
+                update_query = f"UPDATE utenti SET {questions_and_fields[index][1]} = %s WHERE telegram_id = %s"
+                cursor.execute(update_query, (user_response, telegram_user_id))
 
-            # Invio di un messaggio di conferma all'utente
-            confirmation_message = f"{current_field} salvat*: {user_response}"
-            bot.send_message(telegram_user_id, confirmation_message)
+                # Commit delle modifiche al database
+                mysql_connection.commit()
 
-            # Commit delle modifiche al database
-            mysql_connection.commit()
+                # Invio di un messaggio di conferma all'utente
+                confirmation_message = f"{questions_and_fields[index][1]} salvat*: {user_response}"
+                bot.send_message(telegram_user_id, confirmation_message)
 
-            # Chiusura del cursore
-            cursor.close()
-
-            # Aggiornamento dello stato dell'utente
-            state['user_state'][current_field] = True
-
-            # Verifica se ci sono altre domande da fare
-            next_question, next_field = get_next_question(state['user_state'])
-            if next_field:
-                # Fai la prossima domanda
-                bot.send_message(telegram_user_id, next_question)
-                current_question, current_field = next_question, next_field
-            else:
-                # Se tutte le domande sono state risposte, invia un messaggio finale
-                bot.send_message(telegram_user_id, "Grazie per le risposte! Il tuo profilo è completo. Puoi chiedere "
-                                                   "al chatbot ciò che desideri!")
                 # Chiudi la connessione al database
                 mysql_connection.close()
 
-                # Chiudi il cursore
-                cursor.close()
+                # Passa alla prossima domanda se ci sono ancora domande
+                index += 1
+                ask_next_question()
+
+                if index >= len(questions_and_fields):
+                    interview_complete = True
+                    send_completion_message()
+
+
 
         except Exception as e:
             print(f"Errore durante la gestione della risposta: {e}")
 
-    def get_next_question(user_state):
-        for question, field in questions_and_fields:
-            if not user_state[field]:
-                return question, field
-        return None, None
+    # Inizia chiedendo la prima domanda
+    ask_next_question()
 
-    # Inizia con la prima domanda
-    bot.send_message(telegram_id, current_question)
 
-    # Registra l'handler del messaggio per la prima domanda
-    bot.add_message_handler(handle_user_response)
+
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import threading
-import time
 
 from dotenv import load_dotenv
 
@@ -19,9 +18,6 @@ mysql_connection, mysql_cursor = connect_mysql()
 # Evento per fermare il thread
 stop_event = threading.Event()
 
-# Flag per verificare se il thread periodico è già in esecuzione
-periodic_thread_running = False
-
 # Dizionario per tenere traccia dello stato dell'utente
 user_states = {}
 
@@ -30,23 +26,16 @@ telegram_id = None
 
 # Aggiungi una variabile di controllo per indicare se tutte le risposte sono state date
 all_responses_received = False
-
-
-def periodic_task():
-    # Nel loop del thread periodico, controlla se tutte le risposte sono state ricevute
-    while not stop_event.is_set() and not all_responses_received:
-        # Tuo codice per il task periodico qui
-        time.sleep(60)  # Esempio: attesa di 60 secondi tra i cicli
+periodic_thread_running = False
+openai, bot_telegram, root = connect()
 
 
 if __name__ == '__main__':
-    openai, bot_telegram, root = connect()
 
     @bot_telegram.message_handler(commands=[START_COMMAND])
     def send_welcome(message):
         global telegram_id  # Dichiarazione di telegram_id come variabile globale
         telegram_id = message.chat.id
-
         # Check if the user exists in the database
         user_profile = get_user_profile(telegram_id)
 
@@ -60,37 +49,25 @@ if __name__ == '__main__':
         questions_and_fields = [
             ('Qual è la tua età?', 'eta'),
             ('Quali sono le tue patologie o disturbi?', 'malattie'),
-            ('Che sentimento provi mentre mangi o pensi al cibo? Indicalo scrivendo: Positivo, Neutrale, Negativo', 'emozione')
+            ('Che sentimento provi mentre mangi o pensi al cibo? Indicalo scrivendo: Positivo, Neutrale, Negativo',
+             'emozione')
         ]
 
-        for question, field in questions_and_fields:
-            if not user_profile.get(field):
-                # L'utente non ha fornito l'informazione, chiedi le informazioni mancanti
-                user_states[telegram_id] = {'current_question': question, 'current_field': field}
-                ask_user_info(telegram_id, bot_telegram, questions_and_fields, user_states)
-                return
-        # Se tutte le domande sono state risposte, puoi eseguire ulteriori azioni qui
+        ask_user_info(telegram_id, bot_telegram, questions_and_fields)
 
-        @bot_telegram.message_handler(func=lambda message_gpt: message_gpt.chat.id == telegram_id)
-        def handle_user_response(message_gpt):
+
+    @bot_telegram.message_handler(func=lambda message_gpt: message_gpt.chat.id == telegram_id)
+    def handle_user_response_gpt(message_gpt):
+        user_profile = get_user_profile(telegram_id)
+
+        if user_profile.get('eta') and user_profile.get('malattie') and user_profile.get('emozione'):
+            # L'utente esiste nel database, passa il messaggio a ChatGPT
             response_from_chatgpt = write_chatgpt(openai, message_gpt, user_profile)
 
             # Invia la risposta di ChatGPT al bot Telegram
             bot_telegram.send_message(telegram_id, response_from_chatgpt)
 
-            print(f"Tutte le domande sono state risposte da {telegram_id}")
-
-            global all_responses_received
-            all_responses_received = True
 
 
-    # Start del thread periodico
-    if not periodic_thread_running:
-        threading.Thread(target=periodic_task, daemon=True).start()
-        periodic_thread_running = True
-
-    stop_event.set()
-    # Esegui il polling infinito del bot Telegram
-    bot_telegram.infinity_polling()
-
-
+# Esegui il polling infinito del bot Telegram
+bot_telegram.infinity_polling()
